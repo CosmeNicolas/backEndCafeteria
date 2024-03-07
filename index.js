bcf6e3789b4cad76f717435408dbf1ea5de426d8
CosmeNicolas <nicomas7590@gmail.com>
2024-03-07 17:01:49 -0300

    configuramos un archivo estatico, en la ruta principal del proyecto

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //1- configurar un puerto 
 const app = express();
@@ -16,14 +18,24 @@ app.listen(app.get('port'), ()=>{
 app.use(cors())
 //morgan, nos da info extra en la terminal y debemos pasarle un argumento
 app.use(morgan('dev'))
-//agregar el resto de los middlewares
+//middleware de express, q le dafomrato json
+app.use(express.json())
+//express urlencoded- interpreta los datos del body
+app.use(express.urlencoded({extended: true}))
+//
+const __filename = fileURLToPath(import .meta.url)
+const __dirname = path.dirname(__filename)
+/* console.log(__filename)
+console.log(path.join(__dirname,'/public')) */
+app.use(express.static(path.join(__dirname,'/public')))
+
 
 
 
 
 //3- configurar las rutas
-//ex, http://localhost:3000/productos
-app.get('/', (req, res)=>{
+//ex, http://localhost:4001
+app.get('/nuevo', (req, res)=>{
     //agregar toda la logica del back
     console.log('procesando una solicitud get')
     res.send('respuesat del back')
