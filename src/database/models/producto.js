11ffb81b67c70df0362ed121cc26cf2e9e610366
CosmeNicolas <nicomas7590@gmail.com>
2024-03-12 17:48:22 -0300

    agregamos el esquema que tendran nuestros productos

---
+++ src/database/models/producto.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+    unique: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 50,
+    max: 10000,
+    validate: {
+      validator: (valor) =>
+        /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png|jpeg)/.test(valor),
+      message: (dato) => `${dato.value} no es una URL de imagen valida`,
+    },
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: [
+      "infusiones",
+      "batidos",
+      "cafeComun",
+      "cafeEspecialidad",
+      "materiaPrima",
+      "panaderiaDulce",
+      "panaderiasalado",
+      "bocadillos",
+    ],
+  },
+  descripcion_breve: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  descripcion_amplia: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 260,
+  },
+});
+
+// Crear el modelo del producto
+const Producto = mongoose.model("producto", productoSchema);
+
+export default Producto;
