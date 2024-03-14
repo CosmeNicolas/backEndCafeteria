a9a36d10e0ce4065c7b98818ae7145e76cbd2514
CosmeNicolas <nicomas7590@gmail.com>
2024-03-14 17:18:08 -0300

    agregamos la funcion obtener producto , la cual utiliza un id por parametro

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -33,3 +33,21 @@ export const crearProducto = async(req, res)=>{
         })
     }
 }
+
+export const obtenerProducto = async(req, res)=>{
+    try {
+        //extraer el id 
+        console.log(req.params.id)
+        //solicitar a la base de datos, buscar ese producto 
+        const productoBuscado = await Producto.findById(req.params.id)
+        //preguntar si no encontre el producto buscado
+        if(!productoBuscado){
+            return res.status(404).json({mensaje: " No se encontro el producto con el id"})
+        }
+        //enviar respuesta 
+        res.status(200).json(productoBuscado)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({mensaje: "no se encontro el producto solicitado"})
+    }
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { listarProductos, crearProducto } from "../controllers/productos.controllers.js";
+import { listarProductos, crearProducto, obtenerProducto } from "../controllers/productos.controllers.js";
 
 
 const router = Router();
 
 //como creo las rutas
 
-router.route('/productos').get(listarProductos).post(crearProducto)
+router.route('/productos').get(listarProductos).post(crearProducto);
+router.route('/productos/:id').get(obtenerProducto)
 
 export default router 
